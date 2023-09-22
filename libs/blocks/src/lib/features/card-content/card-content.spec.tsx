a719dc99a21130bfab5f689d80b846bcb41b3eb7
Nuzhy-Deriv <nuzhy@deriv.com>
2023-09-22 17:16:49 +0800

    chore: added testcases

--- libs/blocks/src/lib/features/card-content/card-content.spec.tsx
+++ libs/blocks/src/lib/features/card-content/card-content.spec.tsx
@@ -1,10 +1,31 @@
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-import Card from '.';
+import CardContent from '.';
 
-describe('Card', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Card />);
-    expect(baseElement).toBeTruthy();
-  });
+describe('CardContent', () => {
+  it('renders with correct class names and content', () => {
+    const className = 'text-heading-h2';
+    const title = 'This is the title';
+    const description = 'Description here';
+
+    const { getByText } = render(
+      <CardContent
+        className={className}
+        title={title}
+        description={description}
+      ><p>Content</p></CardContent>
+    );
+    const titleElement = getByText(title);
+
+    //renders with correct classname
+    expect(titleElement).toHaveClass(className);
+
+    //renders with the correct title
+    expect(getByText(title)).toBeInTheDocument();
+
+    //renders with the correct description
+    expect(getByText(description)).toBeInTheDocument();
+
+  })
 });
