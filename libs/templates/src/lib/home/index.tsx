d7afea95dda2c74c6c5dfb254c7d20bb31f986a8
meenu-deriv <meenakshi@deriv.com>
2023-10-03 09:29:20 +0400

    chore: removed unused blocks in home template

--- libs/templates/src/lib/home/index.tsx
+++ libs/templates/src/lib/home/index.tsx
@@ -16,68 +16,9 @@ export function HomeTemplate() {
       <HeroBanner />
       <NumberSection />
       <FastAndReliableSection />
-
-      <Hero.ContentLimit
-        title="Title goes here"
-        description={`Description goes here description goes here
-          description goes here description goes here`}
-        content={() => (
-          <img
-            className="w-full"
-            src="https://placehold.co/712x600"
-            alt="Placeholder"
-          />
-        )}
-      >
-        <p>This is a content limit hero</p>
-      </Hero.ContentLimit>
-
       <WhyTradeSection />
-
       <ProvenExcellenceSection />
-
       <StaticNumberBlock />
-
-      <Hero.ContentTop title="Content Top">
-        <img
-          className="w-full"
-          src="https://placehold.co/712x200"
-          alt="Placeholder"
-        />
-      </Hero.ContentTop>
-
-      <Hero.ContentBottom title="Content Bottom">
-        <img
-          className="w-full"
-          src="https://placehold.co/712x200"
-          alt="Placeholder"
-        />
-      </Hero.ContentBottom>
-
-      <Hero.ContentLess
-        title="Title goes here"
-        description={`Description goes here description goes here
-          description goes here description goes here`}
-      >
-        <Text className="text-center">This is a content less block</Text>
-      </Hero.ContentLess>
-
-      <Features.ContentRight
-        title="Content Right Title"
-        description="Description goes here description goes here description goes here description goes here"
-        content={
-          <div className="flex w-full">
-            <Text>Any Content here</Text>
-          </div>
-        }
-        image_content={
-          <img
-            className="flex w-full flex-1"
-            src="https://placehold.co/712x800"
-            alt="Placeholder"
-          />
-        }
-      />
     </PageLayout>
   );
 }
