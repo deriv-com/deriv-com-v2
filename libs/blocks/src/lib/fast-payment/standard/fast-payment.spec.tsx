33bf356be9ac2dbe1d28988b4b03b539fa3f1215
Prince <prince@deriv.com>
2023-10-17 13:13:45 +0400

    chore: add tests

--- libs/blocks/src/lib/fast-payment/standard/fast-payment.spec.tsx
+++ libs/blocks/src/lib/fast-payment/standard/fast-payment.spec.tsx
@@ -1,11 +1,56 @@
 import { render } from '@testing-library/react';
 
 import FastPayment from './index';
+import {
+  FlagAndorraIcon,
+  FlagAustraliaIcon,
+  FlagBurkinaFasoIcon,
+} from '@deriv/quill-icons';
 
 describe('FastPayment', () => {
-  const child = () => <div>test</div>;
   it('should render successfully', () => {
-    const { baseElement } = render(<FastPayment>{child}</FastPayment>);
+    const { baseElement } = render(
+      <FastPayment
+        title="Hassle-free deposits and withdrawals"
+        description="Make instant deposits from 10,000 HKD. Withdraw in minutes."
+        link={{
+          content: 'Learn More',
+          href: '#',
+        }}
+        content={{
+          className: 'h-fit',
+          cards: [
+            {
+              header: '',
+              color: 'gray',
+              align: 'center',
+              size: 'lg',
+              icon: <FlagBurkinaFasoIcon fill="black" iconSize="2xl" />,
+              className: 'h-fit',
+              nonContentClassName: 'p-general-xl',
+            },
+            {
+              header: '',
+              color: 'gray',
+              align: 'center',
+              size: 'lg',
+              icon: <FlagAndorraIcon fill="black" iconSize="2xl" />,
+              className: 'h-fit',
+              nonContentClassName: 'p-general-xl',
+            },
+            {
+              header: '',
+              color: 'gray',
+              align: 'center',
+              size: 'lg',
+              icon: <FlagAustraliaIcon fill="black" iconSize="2xl" />,
+              className: 'h-fit',
+              nonContentClassName: 'p-general-xl',
+            },
+          ],
+        }}
+      />,
+    );
     expect(baseElement).toBeTruthy();
   });
 });
