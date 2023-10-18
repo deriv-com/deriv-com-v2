22ded822bf4bcc7b729c687882aa3a061d556fbe
meenu-deriv <meenakshi@deriv.com>
2023-10-18 12:24:39 +0400

    fix: updated images for desktop and mobile

--- libs/templates/src/lib/home/sections/cta/data.tsx
+++ libs/templates/src/lib/home/sections/cta/data.tsx
@@ -9,15 +9,25 @@ export const CTABlockContent = () => {
       header="Join over 2.5 million online traders"
       color="gray"
       content={
-        <OptimizedImage
-          imageName="home/hero/cta_image.png"
-          alt="home hero"
-          width={712}
-          height={800}
-        />
+        <>
+          <OptimizedImage
+            className="block lg:hidden"
+            imageName="home/hero/cta_responsive.png"
+            width={712}
+            height={800}
+            alt="cta demo forex mobile"
+          />
+          <OptimizedImage
+            className="hidden lg:block"
+            imageName="home/hero/cta_desktop.png"
+            width={712}
+            height={800}
+            alt="cta demo forex desktop"
+          />
+        </>
       }
     >
-      <Button className={qtMerge('lg:mt-200')} size="md">
+      <Button className={qtMerge('lg:mt-400')} size="md">
         Get a demo forex account
       </Button>
     </Card.ContentRight>
