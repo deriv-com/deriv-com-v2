841719d96754eec56a0a9d796ddc2dd3ef33377c
meenu-deriv <meenakshi@deriv.com>
2023-10-19 09:54:50 +0400

    fix: removed separate image for desktop and responsive

--- libs/templates/src/lib/home/sections/cta/data.tsx
+++ libs/templates/src/lib/home/sections/cta/data.tsx
@@ -9,22 +9,13 @@ export const CTABlockContent = () => {
       header="Join over 2.5 million online traders"
       color="gray"
       content={
-        <>
-          <OptimizedImage
-            className="block lg:hidden"
-            imageName="home/hero/cta_responsive.png"
-            width={712}
-            height={800}
-            alt="cta demo forex mobile"
-          />
-          <OptimizedImage
-            className="hidden lg:block"
-            imageName="home/hero/cta_desktop.png"
-            width={712}
-            height={800}
-            alt="cta demo forex desktop"
-          />
-        </>
+        <OptimizedImage
+          className="block"
+          imageName="home/hero/cta_image.png"
+          width={712}
+          height={800}
+          alt="cta demo forex mobile"
+        />
       }
     >
       <Button className="lg:mt-400" size="md">
