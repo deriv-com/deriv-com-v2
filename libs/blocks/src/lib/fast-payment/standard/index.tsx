62a62ad5c345d64bd2fd637c893fc5ece6945071
Prince <prince@deriv.com>
2023-10-24 11:21:24 +0400

    fix: used multiple containers to handle gap properly

--- libs/blocks/src/lib/fast-payment/standard/index.tsx
+++ libs/blocks/src/lib/fast-payment/standard/index.tsx
@@ -26,32 +26,36 @@ const FastPayment: React.FC<FastPaymentProps> = ({
   return (
     <FluidContainer className="bg-background-primary-container py-general-3xl">
       <div className="flex flex-col items-center">
-        <Heading.H2 className="mb-1200">{title}</Heading.H2>
-        {description && (
-          <Text size="xl" className="mb-gap-md">
-            {description}
-          </Text>
-        )}
-        {link && (
-          <CustomLink
-            href={link.href}
-            className="flex items-center justify-center text-300 text-opacity-black-600"
-          >
-            {link.content}
-            <StandaloneChevronRightRegularIcon fill="#000000b8" />
-          </CustomLink>
-        )}
-        <div className="mb-general-lg mt-general-2xl flex w-full justify-center">
-          <CardsContainer
-            cards={cards}
-            variant="ContentBottom"
-            cols={cols}
-            dense={dense}
-          />
+        <div className="flex flex-col gap-2400 max-lg:gap-1600">
+          <div className="flex flex-col gap-1600 max-lg:gap-1200">
+            <div className="flex flex-col items-center justify-center gap-1200 max-sm:gap-800">
+              <Heading.H2>{title}</Heading.H2>
+              {description && <Text size="xl">{description}</Text>}
+            </div>
+            {link && (
+              <CustomLink
+                href={link.href}
+                className="flex items-center justify-center text-300 text-opacity-black-600"
+              >
+                {link.content}
+                <StandaloneChevronRightRegularIcon fill="#000000b8" />
+              </CustomLink>
+            )}
+          </div>
+          <div className="flex flex-col items-center justify-center gap-1200">
+            <div className="flex w-full justify-center">
+              <CardsContainer
+                cards={cards}
+                variant="ContentBottom"
+                cols={cols}
+                dense={dense}
+              />
+            </div>
+            {disclaimer && (
+              <p className="text-100 text-opacity-black-400">{disclaimer}</p>
+            )}
+          </div>
         </div>
-        {disclaimer && (
-          <p className="text-100 text-opacity-black-400">{disclaimer}</p>
-        )}
       </div>
     </FluidContainer>
   );
