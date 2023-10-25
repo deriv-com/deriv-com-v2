90540e2bfe76b78b2104229d7b5c21178dbf6537
Prince <prince@deriv.com>
2023-10-25 09:12:12 +0400

    chore: added trustpilot

--- libs/templates/src/lib/home/index.tsx
+++ libs/templates/src/lib/home/index.tsx
@@ -8,7 +8,7 @@ import WhyTradeSection from './sections/why-trade';
 import ProvenExcellenceSection from './sections/proven-excellence';
 import StaticNumberBlock from './sections/static-number-block';
 import CardSliderSection from './sections/slider-section';
-import AccountSection from './sections/account-section';
+import ReviewSection from './sections/review';
 
 export function HomeTemplate() {
   return (
@@ -17,7 +17,7 @@ export function HomeTemplate() {
       <NumberSection />
       <FastAndReliableSection />
       <WhyTradeSection />
-      <AccountSection />
+      <ReviewSection />
       <ProvenExcellenceSection />
       <CardSliderSection />
       <StaticNumberBlock />
