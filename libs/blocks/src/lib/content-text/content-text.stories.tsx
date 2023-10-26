e6886f7fd50aa3a4e760f1fb1183f48969917432
Jia Wei <jiawei@deriv.com>
2023-10-26 16:27:10 +0800

    feat: Mission Content Text Block

--- libs/blocks/src/lib/content-text/content-text.stories.tsx
+++ libs/blocks/src/lib/content-text/content-text.stories.tsx
@@ -8,32 +8,36 @@ const meta = {
   component: ContentText,
 } satisfies Meta<typeof ContentText>;
 
-const contents = [
-  <Text>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta augue at
-    dui ullamcorper, sed suscipit libero dapibus. Aenean interdum libero ac
-    justo bibendum, a eleifend nulla hendrerit. Cras vel lacus sit amet sem
-    tincidunt fringilla. Suspendisse vel ex ut lectus mattis venenatis. Fusce
-    viverra ligula nec ex scelerisque, a efficitur urna tincidunt. Vivamus ac
-    justo nec velit finibus fermentum.
-  </Text>,
-  <Text>
-    Praesent euismod, nisi ac bibendum facilisis, est purus tincidunt arcu, eget
-    auctor nisi purus eu odio. Nunc nec lacinia justo. Proin auctor, purus sed
-    varius eleifend, dolor metus aliquet sapien, eget tempus justo tortor a dui.
-    Maecenas euismod, purus vel venenatis tempus, justo quam tristique purus, ac
-    consequat erat nisi eu purus. Fusce vitae lorem id lectus auctor malesuada.
-    Nulla facilisi. Vivamus congue tristique tellus, sit amet vehicula purus
-    bibendum quis.
-  </Text>,
-  <Text>
-    Phasellus cursus dignissim lectus, in vestibulum justo tempor in. Nulla
-    facilisi. Sed sit amet dui in justo luctus dictum. Vestibulum malesuada,
-    justo id gravida volutpat, arcu purus scelerisque odio, id lacinia odio arcu
-    in ligula. Integer ut ipsum ut odio cursus sodales. Suspendisse nec
-    fermentum ex. Cras sed lectus quis metus fermentum interdum.
-  </Text>,
-];
+const Content = () => {
+  return (
+    <>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porta augue
+        at dui ullamcorper, sed suscipit libero dapibus. Aenean interdum libero
+        ac justo bibendum, a eleifend nulla hendrerit. Cras vel lacus sit amet
+        sem tincidunt fringilla. Suspendisse vel ex ut lectus mattis venenatis.
+        Fusce viverra ligula nec ex scelerisque, a efficitur urna tincidunt.
+        Vivamus ac justo nec velit finibus fermentum.
+      </Text>
+      <Text>
+        Praesent euismod, nisi ac bibendum facilisis, est purus tincidunt arcu,
+        eget auctor nisi purus eu odio. Nunc nec lacinia justo. Proin auctor,
+        purus sed varius eleifend, dolor metus aliquet sapien, eget tempus justo
+        tortor a dui. Maecenas euismod, purus vel venenatis tempus, justo quam
+        tristique purus, ac consequat erat nisi eu purus. Fusce vitae lorem id
+        lectus auctor malesuada. Nulla facilisi. Vivamus congue tristique
+        tellus, sit amet vehicula purus bibendum quis.
+      </Text>
+      <Text>
+        Phasellus cursus dignissim lectus, in vestibulum justo tempor in. Nulla
+        facilisi. Sed sit amet dui in justo luctus dictum. Vestibulum malesuada,
+        justo id gravida volutpat, arcu purus scelerisque odio, id lacinia odio
+        arcu in ligula. Integer ut ipsum ut odio cursus sodales. Suspendisse nec
+        fermentum ex. Cras sed lectus quis metus fermentum interdum.
+      </Text>
+    </>
+  );
+};
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -43,6 +47,6 @@ export const Default: Story = {
     className:
       'my-1000 bg-background-primary-container max-sm:px-800 max-lg:px-1200',
     title: 'Title here',
-    content: contents,
+    content: <Content />,
   },
 };
---
+++ libs/templates/src/lib/who-we-are/sections/mission-content/index.tsx
@@ -0,0 +1,8 @@
+import { ContentText } from '@deriv-com/blocks';
+import Content from './data';
+
+export const MissionContent = () => {
+  return <ContentText content={<Content />} />;
+};
+
+export default MissionContent;
