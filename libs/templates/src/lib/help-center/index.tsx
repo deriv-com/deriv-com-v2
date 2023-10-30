d4eb3eb3ced1dbcc7310e1b01089e8b636b71883
Prince <prince@deriv.com>
2023-10-30 17:30:34 +0400

    chore: undo unneccessary commit

--- libs/templates/src/lib/help-center/index.tsx
+++ libs/templates/src/lib/help-center/index.tsx
@@ -1,33 +1,11 @@
 import { Hero } from '@deriv-com/blocks';
-import { Breadcrumbs, OptimizedImage, PageLayout } from '@deriv-com/components';
+import { OptimizedImage, PageLayout } from '@deriv-com/components';
 import { FluidContainer } from '@deriv/quill-design';
 
 export function HelpCenterTemplate() {
   return (
     <PageLayout>
       <FluidContainer>
-        <div className="flex py-general-md">
-          <Breadcrumbs
-            links={[
-              {
-                href: '/',
-                content: 'Home',
-              },
-              {
-                href: '/help-centre',
-                content: 'Help Centre',
-              },
-              {
-                href: '/asdasd',
-                content: 'Other Page',
-              },
-              {
-                href: '/asdasd',
-                content: 'And another Page',
-              },
-            ]}
-          />
-        </div>
         <Hero.ContentTop title="How can we help?">
           <OptimizedImage
             imageName="help-centre/hero/hk_help_centre.png"
