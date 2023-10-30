c126c4edf5b80fb34f1f687685215359f34875b1
jia-deriv <jiawei@regentmarkets.com>
2023-10-30 10:26:45 +0800

    feat: app download section

--- libs/templates/src/lib/trade/app-download/index.tsx
+++ libs/templates/src/lib/trade/app-download/index.tsx
@@ -12,29 +12,32 @@ const AppDownload = () => {
       align="start"
       size="lg"
       content={content}
-      className="items-end"
+      className="items-end lg:gap-gap-lg"
     >
-      <Heading.H2>{heading}</Heading.H2>
-      <Text className="text-body-lg leading-body-lg">{description}</Text>
-      <div className="flex w-full flex-col gap-gap-xl md:flex-row lg:flex-col">
-        <div className="flex w-full items-start gap-gap-lg">
-          {cta.qr}
-          <div className="flex flex-col gap-gap-md">
-            <Text>{cta.description}</Text>
-            <Text variant="bold" className="text-body-lg">
-              {cta.devices}
-            </Text>
-          </div>
+      <div className="flex flex-col gap-gap-xl lg:gap-gap-2xl">
+        <div className="flex flex-col gap-gap-xl">
+          <Heading.H2>{heading}</Heading.H2>
+          <Text className="text-body-lg leading-body-lg">{description}</Text>
         </div>
-        <div className="flex w-full flex-wrap gap-gap-lg">
-          {platforms.map((platform, index) => {
-            return (
-              <div className="flex items-center gap-gap-md" key={index}>
-                {platform.icon}
-                <Text>{platform.name}</Text>
-              </div>
-            );
-          })}
+
+        <div className="flex w-full flex-col gap-gap-xl md:flex-row lg:flex-col">
+          <div className="flex w-full items-start gap-gap-lg">
+            {cta.qr}
+            <div className="grid w-[134px] gap-gap-md lg:w-full">
+              <Text>{cta.description}</Text>
+              <Text className="text-body-lg">{cta.devices}</Text>
+            </div>
+          </div>
+          <div className="flex w-full flex-wrap gap-gap-lg">
+            {platforms.map((platform, index) => {
+              return (
+                <div className="flex items-center gap-gap-md" key={index}>
+                  {platform.icon}
+                  <Text>{platform.name}</Text>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Card.ContentRight>
