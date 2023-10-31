933b9d005e7a26abc08f16b768c1fc0486875bc8
Prince <prince@deriv.com>
2023-10-31 08:59:37 +0400

    fix: border issues

--- libs/components/src/lib/accordion/flush/index.tsx
+++ libs/components/src/lib/accordion/flush/index.tsx
@@ -1,11 +1,9 @@
-import styles from './standard.module.css';
-
 import { AccordionProps } from '../types';
 
 import Base from '../base';
 
 export const Flush = ({ ...otherProps }: AccordionProps) => {
-  return <Base divider="bottom" {...otherProps} />;
+  return <Base divider="bottom" className="border-x-none" {...otherProps} />;
 };
 
 export default Flush;
