a0a34dfe3c72a8d400b9e1c0e5e59dbcff5a6aa3
Prince <prince@deriv.com>
2023-10-31 10:45:14 +0400

    chore: added mobile breadcrumbs functionality

--- libs/templates/src/lib/help-center/index.tsx
+++ libs/templates/src/lib/help-center/index.tsx
@@ -1,11 +1,25 @@
 import { Hero } from '@deriv-com/blocks';
-import { OptimizedImage, PageLayout } from '@deriv-com/components';
+import { Breadcrumbs, OptimizedImage, PageLayout } from '@deriv-com/components';
 import { FluidContainer } from '@deriv/quill-design';
 
 export function HelpCenterTemplate() {
   return (
     <PageLayout>
       <FluidContainer>
+        <div className="py-general-md">
+          <Breadcrumbs
+            links={[
+              {
+                content: 'Home',
+                href: '/',
+              },
+              {
+                content: 'Help Centre',
+                href: '/help-centre',
+              },
+            ]}
+          />
+        </div>
         <Hero.ContentTop title="How can we help?">
           <OptimizedImage
             imageName="help-centre/hero/hk_help_centre.png"
