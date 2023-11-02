acfe25762137182d610ac3f783a11f39c02eb2fb
meenu-deriv <meenakshi@deriv.com>
2023-11-02 13:06:32 +0400

    fix: fix conflicts

--- libs/templates/src/lib/home/sections/cta/index.tsx
+++ libs/templates/src/lib/home/sections/cta/index.tsx
@@ -1,9 +1,9 @@
-import { FluidContainer } from '@deriv/quill-design';
+import { FluidContainer, qtMerge } from '@deriv/quill-design';
 import { CTABlockContent } from './data';
 
 const CTASection = () => {
   return (
-    <FluidContainer className="my-2400 flex-none lg:my-3600">
+    <FluidContainer className={qtMerge('my-2400 flex-none lg:my-3600')}>
       <CTABlockContent />
     </FluidContainer>
   );
