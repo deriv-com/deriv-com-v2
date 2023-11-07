e1613a20c9a685e7a4303ced55a59a3aa7a94ae9
jia-deriv <jiawei@regentmarkets.com>
2023-11-07 15:32:48 +0800

    fix: app rendering missiong in trade page

--- libs/templates/src/lib/trade/app-download/index.tsx
+++ libs/templates/src/lib/trade/app-download/index.tsx
@@ -1,5 +1,10 @@
 import React, { HTMLAttributes } from 'react';
-import { BodyTypographyProps, Heading, Text } from '@deriv/quill-design';
+import {
+  BodyTypographyProps,
+  FluidContainer,
+  Heading,
+  Text,
+} from '@deriv/quill-design';
 import { Card } from '@deriv-com/components';
 import { data } from './data';
 import clsx from 'clsx';
@@ -24,46 +29,48 @@ const AppDownload = () => {
   };
 
   return (
-    <Card.ContentRight
-      color="gray"
-      align="start"
-      size="lg"
-      content={content}
-      contentClassName="w-full flex justify-center"
-      className="lg:gap-gap-lg"
-    >
-      <CustomDiv className="flex-col gap-gap-2xl">
-        <CustomDiv className="flex-col gap-gap-xl">
-          <Heading.H2>{heading}</Heading.H2>
-          <CustomText className="text-body-lg leading-body-lg">
-            {description}
-          </CustomText>
-        </CustomDiv>
+    <FluidContainer>
+      <Card.ContentRight
+        color="gray"
+        align="start"
+        size="lg"
+        content={content}
+        contentClassName="w-full flex justify-center"
+        className="lg:gap-gap-lg"
+      >
+        <CustomDiv className="flex-col gap-gap-2xl">
+          <CustomDiv className="flex-col gap-gap-xl">
+            <Heading.H2>{heading}</Heading.H2>
+            <CustomText className="text-body-lg leading-body-lg">
+              {description}
+            </CustomText>
+          </CustomDiv>
 
-        <CustomDiv className="w-full flex-col gap-gap-xl md:flex-row lg:flex-col">
-          <CustomDiv className="w-full items-start gap-gap-lg">
-            {cta.qr}
-            <div className="grid w-[134px] gap-gap-md lg:w-full">
-              <CustomText>{cta.description}</CustomText>
-              <CustomText bold className="text-body-lg">
-                {cta.devices}
-              </CustomText>
-            </div>
-          </CustomDiv>
-          <CustomDiv className="w-full flex-wrap gap-gap-lg">
-            {platforms.map((platform) => (
-              <CustomDiv
-                className="items-center gap-gap-md"
-                key={platform.name}
-              >
-                {platform.icon}
-                <Text>{platform.name}</Text>
-              </CustomDiv>
-            ))}
+          <CustomDiv className="w-full flex-col gap-gap-xl md:flex-row lg:flex-col">
+            <CustomDiv className="w-full items-start gap-gap-lg">
+              {cta.qr}
+              <div className="grid w-[134px] gap-gap-md lg:w-full">
+                <CustomText>{cta.description}</CustomText>
+                <CustomText bold className="text-body-lg">
+                  {cta.devices}
+                </CustomText>
+              </div>
+            </CustomDiv>
+            <CustomDiv className="w-full flex-wrap gap-gap-lg">
+              {platforms.map((platform) => (
+                <CustomDiv
+                  className="items-center gap-gap-md"
+                  key={platform.name}
+                >
+                  {platform.icon}
+                  <Text>{platform.name}</Text>
+                </CustomDiv>
+              ))}
+            </CustomDiv>
           </CustomDiv>
         </CustomDiv>
-      </CustomDiv>
-    </Card.ContentRight>
+      </Card.ContentRight>
+    </FluidContainer>
   );
 };
 
