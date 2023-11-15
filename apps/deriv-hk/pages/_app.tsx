2272a998930b00ee9f196e88ad37db169bed9e29
Michio <mohsen@regentmarkets.com>
2023-11-15 11:25:11 +0800

    chore: updated head in custom app

--- apps/deriv-hk/pages/_app.tsx
+++ apps/deriv-hk/pages/_app.tsx
@@ -2,13 +2,23 @@ import { AppProps } from 'next/app';
 import '../styles.css';
 import { BreakpointProvider, ThemeProvider } from '@deriv/quill-design';
 import { BuildVariantProvider } from '@deriv-com/providers';
+import Head from 'next/head';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <BuildVariantProvider buildVariant="hk">
       <BreakpointProvider>
         <ThemeProvider theme="light">
-          <Component {...pageProps} />
+          <>
+            <Head>
+              <meta name="color-scheme" content="light dark" />
+              <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+              />
+            </Head>
+            <Component {...pageProps} />
+          </>
         </ThemeProvider>
       </BreakpointProvider>
     </BuildVariantProvider>
