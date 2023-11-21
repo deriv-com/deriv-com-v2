1aed334c0ebc7353cbe913b2626f9d34fb109d1a
meenu-deriv <meenakshi@deriv.com>
2023-11-21 18:00:38 +0400

    fix: fixed storybook

--- libs/blocks/src/lib/search/search-block.stories.tsx
+++ libs/blocks/src/lib/search/search-block.stories.tsx
@@ -1,71 +1,17 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import SearchBlock from '.';
-import { Text } from '@deriv/quill-design';
-import { CustomLink } from '@deriv-com/components';
+import { Meta, StoryObj } from '@storybook/react';
+import {
+  SearchResults,
+  SearchResultsProps,
+} from 'libs/templates/src/lib/help-center/sections/search/search-results';
 
-const meta = {
+const meta: Meta<SearchResultsProps> = {
   title: 'Blocks/SearchBlock',
-  component: SearchBlock,
-} satisfies Meta<typeof SearchBlock>;
+  component: SearchResults,
+} satisfies Meta<typeof SearchResults>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const handleSearchKeyChange = (value: string) => {
-  return value;
-};
-
-const searchKey = 'email';
-const noSearchKey = 'asdd';
-const results = [
-  {
-    header: 'Account',
-    questions: [
-      'Can I change my email address?',
-      'How do I unsubscribe from marketing emails?',
-    ],
-  },
-];
-
 export const Default: Story = {
-  args: {
-    placeholder: 'Try “Trade“',
-    onChange: handleSearchKeyChange,
-    showSearchResults: handleSearchKeyChange,
-    content: (
-      <div className="flex flex-col items-start">
-        <Text size="lg" bold className="w-full break-all py-general-xl">
-          Results for “{searchKey}”
-        </Text>
-        {results.length > 0 && (
-          <div className="flex flex-col gap-general-lg pb-2400">
-            {results.map(({ questions }) =>
-              questions.map((question) => {
-                return (
-                  <CustomLink key={question} href="" className="justify-start">
-                    {question}
-                  </CustomLink>
-                );
-              }),
-            )}
-          </div>
-        )}
-      </div>
-    ),
-  },
-};
-
-export const NoResults: Story = {
-  args: {
-    placeholder: 'Try “Trade“',
-    showSearchResults: handleSearchKeyChange,
-    onChange: handleSearchKeyChange,
-    content: (
-      <div className="flex w-full justify-center py-general-2xl">
-        <Text size="lg" className="break-all">
-          Sorry, we couldn’t find any results with "{noSearchKey}" in it
-        </Text>
-      </div>
-    ),
-  },
+  args: {},
 };
