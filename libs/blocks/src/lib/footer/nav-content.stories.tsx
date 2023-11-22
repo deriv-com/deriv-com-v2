2c527fdf64856f3ca095e1717ee05dc5a62a3a87
jia-deriv <jiawei@regentmarkets.com>
2023-11-22 15:44:39 +0800

    chore: improve storybook documentation

--- libs/blocks/src/lib/footer/nav-content.stories.tsx
+++ libs/blocks/src/lib/footer/nav-content.stories.tsx
@@ -1,8 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Footer, { FooterItems } from '.';
-// import { footerData } from 'libs/features/src/lib/footer/main/data';
 
-const footerData: FooterItems[] = [
+const data: FooterItems[] = [
   {
     id: 0,
     layers: [
@@ -13,16 +12,87 @@ const footerData: FooterItems[] = [
         items: [
           {
             id: 0,
-            href: '/who-we-are',
+            href: '/',
             text: 'Who we are',
           },
           {
             id: 1,
-            href: '/why-choose-us',
+            href: '/',
             text: 'Why choose us',
           },
           { id: 2, href: '/contact-us', text: 'Contact us' },
           { id: 3, href: '/principles', text: 'Principles' },
+        ],
+      },
+    ],
+  },
+  {
+    id: 1,
+    layers: [
+      {
+        id: 0,
+        type: 'link',
+        header: 'TRADE',
+        items: [
+          {
+            id: 0,
+            href: '/',
+            text: 'Leveraged forex (Deriv MT5)',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 2,
+    layers: [
+      {
+        id: 0,
+        type: 'link',
+        header: 'LEGAL',
+        items: [
+          {
+            id: 0,
+            href: '/',
+            text: 'Regulatory information',
+          },
+          {
+            id: 1,
+            href: '/',
+            text: 'Terms & conditions',
+          },
+          {
+            id: 2,
+            href: '/',
+            text: 'Secure & responsible trading',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    layers: [
+      {
+        id: 0,
+        type: 'link',
+        header: 'SUPPORT',
+        items: [
+          {
+            id: 0,
+            href: '/',
+            text: 'Help centre',
+          },
+          {
+            id: 1,
+            href: '/',
+            text: 'Payment methods',
+          },
+          {
+            id: 2,
+            href: '/',
+            text: 'Status page',
+          },
         ],
       },
     ],
@@ -40,14 +110,14 @@ type Story = StoryObj<typeof meta>;
 
 export const FourColumn: Story = {
   args: {
-    items: footerData,
+    items: data,
     cols: 'four',
   },
 };
 
 export const SixColumn: Story = {
   args: {
-    items: footerData,
+    ...FourColumn.args,
     cols: 'six',
   },
 };
