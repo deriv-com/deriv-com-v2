81a4f0cbc08f0f9f7a3b71b761be0a33e39e3a0f
jia-deriv <jiawei@regentmarkets.com>
2023-11-24 15:17:01 +0800

    chore: update branch

--- libs/blocks/src/lib/accordion/accordion.stories.tsx
+++ libs/blocks/src/lib/accordion/accordion.stories.tsx
@@ -6,19 +6,18 @@ import { StandaloneAndroidIcon } from '@deriv/quill-icons/Standalone';
 const meta = {
   title: 'Blocks/Accordion',
   component: AccordionBlock,
-  tags: ['autodocs'],
+} satisfies Meta<typeof AccordionBlock>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
   argTypes: {
     variant: {
       options: ['Flush', 'Fill', 'Outline', 'Elevate'],
       control: { type: 'radio' },
     },
   },
-} satisfies Meta<typeof AccordionBlock>;
-
-export default meta;
-type Story = StoryObj<typeof AccordionBlock>;
-
-export const Flush: Story = {
   args: {
     className: '',
     title: 'Title goes here',
@@ -27,34 +26,8 @@ export const Flush: Story = {
       data: [
         {
           icon: <StandaloneAndroidIcon />,
-          title: 'Title 1',
-          subtitle: 'Subtitle 1',
-          className: 'border-opacity-black-100 border-x-none',
-          content: () => (
-            <img
-              className="flex w-full flex-1"
-              src="https://placehold.co/640x320"
-              alt="Placeholder"
-            />
-          ),
-        },
-        {
-          icon: <StandaloneAndroidIcon />,
-          title: 'Title 2',
-          subtitle: 'Subtitle 2',
-          className: 'border-opacity-black-100 border-x-none',
-          content: () => (
-            <img
-              className="flex w-full flex-1"
-              src="https://placehold.co/640x320"
-              alt="Placeholder"
-            />
-          ),
-        },
-        {
-          icon: <StandaloneAndroidIcon />,
-          title: 'Title 3',
-          subtitle: 'Subtitle 3',
+          title: 'This is the title',
+          subtitle: 'Subtitle goes here',
           className: 'border-opacity-black-100 border-x-none',
           content: () => (
             <img
@@ -66,26 +39,5 @@ export const Flush: Story = {
         },
       ],
     },
-  },
-};
-
-export const Fill: Story = {
-  args: {
-    ...Flush.args,
-    variant: 'Fill',
-  },
-};
-
-export const Outline: Story = {
-  args: {
-    ...Flush.args,
-    variant: 'Outline',
-  },
-};
-
-export const Elevate: Story = {
-  args: {
-    ...Flush.args,
-    variant: 'Elevate',
   },
 };
