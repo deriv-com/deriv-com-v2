17907dd405f1ba9de02d2cee92c66fa1424e47f3
meenu-deriv <meenakshi@deriv.com>
2023-11-27 12:25:25 +0400

    fix: chromatic issues fix on live price block

--- libs/blocks/src/lib/live-market/index.tsx
+++ libs/blocks/src/lib/live-market/index.tsx
@@ -41,22 +41,19 @@ export const LiveMarket = <T extends CardVariantType>({
       )}
     >
       <FluidContainer className="flex flex-col items-center justify-center gap-gap-3xl">
-        {(header || description || TabHeader) && (
-          <HeaderSection
-            header={header}
-            description={description}
-            TabHeader={TabHeader}
-          />
+        {(header || TabHeader) && (
+          <HeaderSection header={header} TabHeader={TabHeader} />
         )}
         {
           // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           // @ts-ignore
           <CardSlider {...cardSliderProps} />
         }
+        {description && <Text className="text-center">{description}</Text>}
         {link && (
           <CustomLink
             href={link.href}
-            className={clsx('flex items-center justify-center text-body-lg')}
+            className={clsx('flex items-center justify-center text-body-md')}
           >
             {link.content}
             <StandaloneChevronRightRegularIcon className="text-solid-slate-1400" />
