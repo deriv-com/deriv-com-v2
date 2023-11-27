e92a0266144b9210a3ccfe3a0a335cf926732b05
Michio <mohsen@regentmarkets.com>
2023-11-27 17:31:04 +0800

    chore: updated usage of stat block

--- libs/blocks/src/lib/stat/index.tsx
+++ libs/blocks/src/lib/stat/index.tsx
@@ -1,10 +1,12 @@
 import { ReactNode } from 'react';
 import V1StatBlock from './v1';
 import V2StatBlock from './v2';
+import V3StatBlock from './v3';
 
 export type StatBlockVariants = {
   V1: typeof V1StatBlock;
   V2: typeof V2StatBlock;
+  V3: typeof V3StatBlock;
 };
 
 export const StatBlock: StatBlockVariants = ({
@@ -17,5 +19,6 @@ export const StatBlock: StatBlockVariants = ({
 
 StatBlock.V1 = V1StatBlock;
 StatBlock.V2 = V2StatBlock;
+StatBlock.V3 = V3StatBlock;
 
 export default StatBlock;
