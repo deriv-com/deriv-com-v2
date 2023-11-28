1c2b92c96a26ba7276ac94b18bb0dc55fb29deeb
meenu-deriv <meenakshi@deriv.com>
2023-11-28 11:58:39 +0400

    fix: chromatic issue fixes for content limit and limitless

--- libs/blocks/src/lib/hero/content-less/index.tsx
+++ libs/blocks/src/lib/hero/content-less/index.tsx
@@ -17,13 +17,9 @@ const ContentLess = ({
   return (
     <Section className={clsx('py-general-4xl', className)}>
       <FluidContainer className="flex flex-col gap-gap-3xl lg:px-general-none">
-        <div className="flex flex-col gap-gap-2xl">
+        <div className="flex flex-col gap-gap-lg">
           <Heading.H1 className="text-center">{title}</Heading.H1>
-          {description && (
-            <Text size="xl" className="text-center text-typography-default">
-              {description}
-            </Text>
-          )}
+          {description && <Text className="text-center">{description}</Text>}
         </div>
 
         {children}
