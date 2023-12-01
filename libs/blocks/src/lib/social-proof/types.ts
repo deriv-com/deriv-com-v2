e3b8f55153a85db73c6b77ac4f7a692cc30fe292
prince-deriv <prince@deriv.com>
2023-12-01 08:50:29 +0400

    fix: build issues

--- libs/blocks/src/lib/social-proof/types.ts
+++ libs/blocks/src/lib/social-proof/types.ts
@@ -11,7 +11,7 @@ export interface SocialProofCustomThemeProps {
 
 export interface SocialProofProps {
   theme?: SocialProofCustomThemeProps;
-  content?: ({ ...props }: TPilotDataProps) => [string | ReactNode];
+  content?: ({ ...props }: TPilotDataProps) => string[] | ReactNode[];
   className?: string;
   logo: ReactNode;
   staticData?: TPilotDataProps | null;
