7e5e14c8456c0d605b17e66d3c443084c6470da9
Michio <mohsen@regentmarkets.com>
2023-12-13 14:31:37 +0800

    chore: testing spline

--- libs/templates/src/lib/who-we-are/sections/hero/index.tsx
+++ libs/templates/src/lib/who-we-are/sections/hero/index.tsx
@@ -1,17 +1,21 @@
 import { Hero } from '@deriv-com/blocks';
 import { OptimizedImage } from '@deriv-com/components';
+import Spline from '@splinetool/react-spline';
 
 export const WhoWeAreHero = () => {
   return (
-    <Hero.ContentBottom title="Who we are">
-      <OptimizedImage
-        imageName="who-we-are/hero/hk_who_we_are.png"
-        className="rounded-xl"
-        alt="Who we are"
-        width={1232}
-        height={711}
-      />
-    </Hero.ContentBottom>
+    <Spline scene="https://prod.spline.design/Y-0cdqopNabRR129/scene.splinecode" />
+
+    // <Hero.ContentBottom title="Who we are">
+    //   {/* <OptimizedImage
+    //     imageName="who-we-are/hero/hk_who_we_are.png"
+    //     className="rounded-xl"
+    //     alt="Who we are"
+    //     width={1232}
+    //     height={711}
+    //   /> */}
+    //   <Spline scene="https://prod.spline.design/Y-0cdqopNabRR129/scene.splinecode" />
+    // </Hero.ContentBottom>
   );
 };
 
