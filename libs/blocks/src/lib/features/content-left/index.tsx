8c1455215aad34627f4a1e104c5ea1e14d571d4f
prince-deriv <prince@deriv.com>
2024-01-04 15:38:58 +0400

    chore: package deployment fix

--- libs/blocks/src/lib/features/content-left/index.tsx
+++ libs/blocks/src/lib/features/content-left/index.tsx
@@ -11,7 +11,7 @@ export interface ContentLeftProps {
   variant?: 'content-left' | 'content-right';
 }
 
-// trigger deployment - 4
+// trigger deployment - 5
 const ContentLeft = ({
   title,
   className,
