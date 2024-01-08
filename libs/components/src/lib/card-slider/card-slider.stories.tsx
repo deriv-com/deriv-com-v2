8484106acce18f749d964a2b72a3ef7d79d97560
meenu-deriv <meenakshi@regentmarkets.com>
2024-01-08 17:53:18 +0400

    fix: added min height in storybook

--- libs/components/src/lib/card-slider/card-slider.stories.tsx
+++ libs/components/src/lib/card-slider/card-slider.stories.tsx
@@ -27,8 +27,9 @@ export const Default: Story = {
       {
         id: 1,
         header: 'Card 1',
+        minHeight: 'min-h-[240px]',
         description:
-          'Description here. Description here. Description here. Description here.',
+          'Description here. Description here. Description here. Description here. Description here. Description here. Description here. Description here.',
         icon: <IllustrativeProtectedAndSecureIcon />,
         color: 'gray',
         align: 'start',
@@ -37,6 +38,7 @@ export const Default: Story = {
       {
         id: 2,
         header: 'Card 2',
+        minHeight: 'min-h-[240px]',
         description:
           'Description here. Description here. Description here. Description here.',
         icon: <IllustrativeSupport247Icon />,
@@ -47,6 +49,7 @@ export const Default: Story = {
       {
         id: 3,
         header: 'Card 3',
+        minHeight: 'min-h-[240px]',
         description:
           'Description here. Description here. Description here. Description here.',
         icon: <IllustrativeLicensedAndRegulatedIcon />,
@@ -57,6 +60,7 @@ export const Default: Story = {
       {
         id: 4,
         header: 'Card 4',
+        minHeight: 'min-h-[240px]',
         description:
           'Description here. Description here. Description here. Description here.',
         icon: <IllustrativeSpreadsIcon />,
@@ -67,6 +71,7 @@ export const Default: Story = {
       {
         id: 5,
         header: 'Card 5',
+        minHeight: 'min-h-[240px]',
         description:
           'Description here. Description here. Description here. Description here.',
         icon: <IllustrativeLicensedAndRegulatedIcon />,
