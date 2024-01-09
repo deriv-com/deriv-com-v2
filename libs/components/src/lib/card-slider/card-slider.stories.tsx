4fd2f8e85900a259e6a4d34712d08d942cc2e06f
meenu-deriv <meenakshi@regentmarkets.com>
2024-01-09 12:33:06 +0400

    fix: modified storybook

--- libs/components/src/lib/card-slider/card-slider.stories.tsx
+++ libs/components/src/lib/card-slider/card-slider.stories.tsx
@@ -27,6 +27,70 @@ export const Default: Story = {
       {
         id: 1,
         header: 'Card 1',
+
+        description:
+          'Description here. Description here. Description here. Description here. Description here. Description here.',
+        icon: <IllustrativeProtectedAndSecureIcon />,
+        color: 'gray',
+        align: 'start',
+        size: 'sm',
+      },
+      {
+        id: 2,
+        header: 'Card 2',
+
+        description:
+          'Description here. Description here. Description here. Description here.',
+        icon: <IllustrativeSupport247Icon />,
+        color: 'gray',
+        align: 'start',
+        size: 'sm',
+      },
+      {
+        id: 3,
+        header: 'Card 3',
+
+        description:
+          'Description here. Description here. Description here. Description here.',
+        icon: <IllustrativeLicensedAndRegulatedIcon />,
+        color: 'gray',
+        align: 'start',
+        size: 'sm',
+      },
+      {
+        id: 4,
+        header: 'Card 4',
+
+        description:
+          'Description here. Description here. Description here. Description here.',
+        icon: <IllustrativeSpreadsIcon />,
+        color: 'gray',
+        align: 'start',
+        size: 'sm',
+      },
+      {
+        id: 5,
+        header: 'Card 5',
+        description:
+          'Description here. Description here. Description here. Description here.',
+        icon: <IllustrativeLicensedAndRegulatedIcon />,
+        color: 'gray',
+        align: 'start',
+        size: 'sm',
+      },
+    ],
+  },
+};
+
+export const cardsWithMinHeight: Story = {
+  args: {
+    className: '!py-2000 bg-background-primary-container',
+    slideClasses: 'max-w-xs',
+    variant: 'ContentBottom',
+    cards: [
+      {
+        id: 1,
+        header: 'Card 1',
         minHeight: 'min-h-[240px]',
         description:
           'Description here. Description here. Description here. Description here. Description here. Description here. Description here. Description here.',
