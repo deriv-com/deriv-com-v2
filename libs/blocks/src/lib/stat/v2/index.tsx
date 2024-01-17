3cb9039cb6a3e67e2e783c04ad7d49b2d0effb87
hasan-deriv <abuhasan@deriv.com>
2024-01-17 11:58:05 +0800

    chore: added sr-only heading to stat v2 block

--- libs/blocks/src/lib/stat/v2/index.tsx
+++ libs/blocks/src/lib/stat/v2/index.tsx
@@ -13,6 +13,8 @@ export const V2StatBlock: React.FC<V2StatBlockProps> = ({
 }) => {
   return (
     <Section className={clsx('bg-solid-slate-50 py-general-xl', className)}>
+      {/* This heading is to fix `Heading elements are not in a sequentially-descending order` */}
+      <h2 className="sr-only">Stat Section</h2>
       <FluidContainer>
         <CardsContainer variant="StatCompany" cards={cards} cols="five" />
       </FluidContainer>
