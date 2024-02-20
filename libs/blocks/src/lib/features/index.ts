fb302d35e8c1331c0128ee98c47e91a49e86f91b
jia-deriv <jiawei@regentmarkets.com>
2024-02-20 12:45:00 +0800

    chore: added export to features block index

--- libs/blocks/src/lib/features/index.ts
+++ libs/blocks/src/lib/features/index.ts
@@ -3,14 +3,16 @@ import ContentLeft from './content-left';
 import ContentRight from './content-right';
 import Card from './card-feature';
 import ContentSlider from './content-slider';
-// import ContentLeftSmall from './content-left-small';
+import FeaturesV4 from './features-v4';
+import FeaturesV5 from './features-v5';
 
 type FeatureVariants = {
   Card: typeof Card;
   ContentLeft: typeof ContentLeft;
-  // ContentLeftSmall: typeof ContentLeftSmall;
   ContentRight: typeof ContentRight;
   ContentSlider: typeof ContentSlider;
+  V4: typeof FeaturesV4;
+  V5: typeof FeaturesV5;
 };
 
 export const Features: FeatureVariants = ({
@@ -23,9 +25,10 @@ export const Features: FeatureVariants = ({
 
 Features.Card = Card;
 Features.ContentLeft = ContentLeft;
-// Features.ContentLeftSmall = ContentLeftSmall;
 Features.ContentRight = ContentRight;
 Features.ContentSlider = ContentSlider;
+Features.V4 = FeaturesV4;
+Features.V5 = FeaturesV5;
 
 export default Features;
 
