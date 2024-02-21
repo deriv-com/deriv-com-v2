2daa2b9f0ee95b72939a02d976c7da5c6959e67c
Nuzhy-Deriv <nuzhy@deriv.com>
2024-02-21 13:28:14 +0800

    chore: hero block variant for homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,10 @@ module.exports = {
         slide: '50s slide infinite linear',
         slideRtl: '50s slideRtl infinite linear',
       },
+      backgroundImage: (theme) => ({
+        'gradient-hero': `linear-gradient(84deg, rgba(0, 0, 0, 0.80) 6.97%, rgba(0, 0, 0, 0.00) 107.22%)`,
+        'gradient-hero-mobile': `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 1.45%, rgba(0, 0, 0, 0.00) 34.13%, rgba(15, 10, 10, 0.80) 86.38%)`,
+      }),
     },
   },
   presets: [require('@deriv/quill-design/quill-tailwind/tailwind.config.cjs')],
