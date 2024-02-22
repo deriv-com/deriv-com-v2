91b5903c6117a281244a0f9f5bb0516ca061d90d
Nuzhy-Deriv <nuzhy@deriv.com>
2024-02-22 12:45:12 +0800

    fix: update home hero block

--- libs/blocks/src/lib/hero/home-hero/index.tsx
+++ libs/blocks/src/lib/hero/home-hero/index.tsx
@@ -9,6 +9,7 @@ const HomeHero = ({
   ctaButton: CTAButton,
   awards: Awards,
   heroImage: HeroImage,
+  heroImageClass,
 }: HomeHeroProps) => {
   return (
     <Section
@@ -29,17 +30,17 @@ const HomeHero = ({
               {description}
             </Text>
           )}
-          <div className="flex pt-general-2xl max-sm:justify-center">
+          <div className="flex pt-general-2xl max-sm:justify-center max-sm:pb-general-2xl">
             {CTAButton}
           </div>
         </div>
         {Awards && (
-          <div className="bottom-50 z-10 flex pb-general-xl max-sm:py-general-2xl sm:absolute">
-            {Awards}
+          <div className="bottom-50 z-20 flex pb-general-xl max-sm:justify-center max-sm:pb-general-2xl sm:absolute">
+            <div className="flex gap-gap-xl">{Awards}</div>
           </div>
         )}
       </FluidContainer>
-      {HeroImage && HeroImage}
+      {HeroImage && <div className={heroImageClass}>{HeroImage}</div>}
       <div className="absolute inset-50 z-10 bg-gradient-hero-mobile sm:-z-10 sm:bg-gradient-hero"></div>
     </Section>
   );
