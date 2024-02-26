aa946749d05060a192773da72a6eb4a1a76587ad
Nuzhy-Deriv <nuzhy@deriv.com>
2024-02-26 16:39:34 +0800

    fix: remove nav dropdown content lazy loading

--- libs/blocks/src/lib/navigation/mobile/index.tsx
+++ libs/blocks/src/lib/navigation/mobile/index.tsx
@@ -1,9 +1,10 @@
 import { FluidContainer } from '@deriv/quill-design';
 import NavMobileWrapper from './mobile.wrapper';
 import MobileHeader from './mobile.header';
+import MobileNavList from './mobile.nav-list';
+import MobileNavContentContainer from './mobile.nav-content.container';
 import { NavigationProps } from '..';
 import MobileLanguageToggler from '../language-switcher/mobile-language';
-import { Suspense, lazy } from 'react';
 
 export const MobileNav: React.FC<NavigationProps> = ({
   renderLogo,
@@ -11,10 +12,6 @@ export const MobileNav: React.FC<NavigationProps> = ({
   hasLanguageSwitch,
   topNavigation,
 }) => {
-  const LazyMobileNavList = lazy(() => import('./mobile.nav-list'));
-  const LazyMobileNavContent = lazy(
-    () => import('./mobile.nav-content.container'),
-  );
   return (
     <FluidContainer
       className="fixed z-50 w-screen bg-background-primary-container lg:hidden"
@@ -27,10 +24,8 @@ export const MobileNav: React.FC<NavigationProps> = ({
           <div className="flex h-full flex-col justify-between">
             <MobileHeader />
             <div className="relative flex flex-1 flex-col justify-between px-general-2xl pb-general-lg sm:pb-general-md">
-              <Suspense>
-                <LazyMobileNavList />
-                <LazyMobileNavContent />
-              </Suspense>
+              <MobileNavList />
+              <MobileNavContentContainer />
               {hasLanguageSwitch && <MobileLanguageToggler />}
             </div>
           </div>
