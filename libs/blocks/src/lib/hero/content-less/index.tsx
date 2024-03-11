a23f260528b4675c83d084acfd4fb3fd43c10951
Muhammad Fasih Ali Naqvi <muhammadfasih@Muhammad-Fasihs-Mac-X7KDC4NKQ0-Standard.local>
2024-03-11 14:48:07 +0800

    chore: added heading option and removed content

--- libs/blocks/src/lib/hero/content-less/index.tsx
+++ libs/blocks/src/lib/hero/content-less/index.tsx
@@ -1,4 +1,3 @@
-import Content from '../content';
 import BlockWrapper from '../../block-wrapper';
 import { HeroBlockProps } from '..';
 
@@ -9,10 +8,14 @@ const ContentLess = ({
   children,
 }: HeroBlockProps) => {
   return (
-    <BlockWrapper className={className} background="light">
-      <Content title={title} description={description} center={true}>
-        {children}
-      </Content>
+    <BlockWrapper
+      title={title}
+      description={description}
+      heading="H1"
+      className={className}
+      background="light"
+    >
+      {children}
     </BlockWrapper>
   );
 };
