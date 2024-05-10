ca690da8699001d564c3f1c91e43c8b4b93e3175
Nuzhy-Deriv <nuzhy@deriv.com>
2024-05-10 19:18:47 +0800

    fix: update next version

--- apps/deriv-hk/next.config.js
+++ apps/deriv-hk/next.config.js
@@ -13,6 +13,7 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  output: 'export',
   images: {
     loader: 'custom',
     imageSizes: [10, 256, 384],
